6d241855c401912e024131f3362fe2d3a9b609e8
Mikhail <m.v.bobrov@yandex.ru>
2018-10-30 23:57:30 +0300

    New gui integration

--- scenes/main_menu.lua
+++ scenes/main_menu.lua
@@ -1,8 +1,9 @@
-assets = require("assets")
+local assets = require("assets")
 
 local scm = require("scenes_manager")
 local gui = require("love2d-gui")
 local Button = require("love2d-gui.Button")
+local HLayout = require("love2d-gui.HLayout")
 
 local main_menu = scm.Scene()
 
@@ -11,25 +12,29 @@ local height = love.graphics.getHeight()
 local cs = love.graphics.newCanvas(width, height)
 local b_font = assets.fonts.buttons_font
 
-function draw_bg()
-  local color = {love.graphics.getColor()}
-  -- love.graphics.setColor{0, 0, 0, 1}
-  love.graphics.rectangle("fill", 0, 0, width, height)
-  love.graphics.setColor{0, 0, 0, 1}
-  love.graphics.rectangle("fill", 0, height/2, width, height/2)
-  love.graphics.setColor(color)
-end
+-- function draw_bg()
+--   local color = {love.graphics.getColor()}
+--   -- love.graphics.setColor{0, 0, 0, 1}
+--   love.graphics.rectangle("fill", 0, 0, width, height)
+--   love.graphics.setColor{0, 0, 0, 1}
+--   love.graphics.rectangle("fill", 0, height/2, width, height/2)
+--   love.graphics.setColor(color)
+-- end
 
 function main_menu:init()
+  gui.load()
   t = 0
   local b1 = Button()
+  local b2 = Button()
+  local hl = HLayout()
   b1:resize(800,10)
-  b1:move(0, 100)
   b1:set_font(b_font)
-  b2 = Button()
-  b2:resize(800,10)
-  b2:move(0, 150)
   b2:set_font(b_font)
+  hl:add_widget(b1)
+  hl:add_widget(b2)
+  hl:resize(800, 300)
+  hl:move(0, 100)
+  gui.layout:add_widget(hl)
   function love.mousemoved(x, y, dx, dy)
     gui.mousemoved(x, y, dx, dy)
     b1:set_text("Cursor pos: "..x.." "..y)
@@ -46,7 +51,7 @@ end
 
 function main_menu:draw()
   love.graphics.setCanvas(cs)
-  draw_bg()
+  -- draw_bg()
   gui.draw()
   love.graphics.setShader(assets.shaders.borders)
   love.graphics.setCanvas()
